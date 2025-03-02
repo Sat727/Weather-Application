e1370b5f528a0c8cc4d0ca6cb798b4ce5fc61b1c
Sat727 <seanaturner727@gmail.com>
2025-03-02 03:40:55 -0500

    Progressive XML

--- app/src/main/java/com/b0nggo/weatherapp/WeatherDisplay.java
+++ app/src/main/java/com/b0nggo/weatherapp/WeatherDisplay.java
@@ -1,23 +1,35 @@
 package com.b0nggo.weatherapp;
 
-import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import com.b0nggo.weatherapp.databinding.ActivityWeatherDisplayBinding;
+
+public class WeatherDisplay extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityWeatherDisplayBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_weather_display);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.weather_display_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -25,10 +37,6 @@ public class MainActivity extends AppCompatActivity {
         //View bsubmit = findViewById(R.id.bapply);
 
 
+    }
 
-    }
-    public void submitData(View v) {
-        Intent intent = new Intent(MainActivity.this, WeatherDisplay.class);
-        startActivity(intent);
-    }
 }
